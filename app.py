d1e75613a7d530c01cad01f0447496e4de4816d4
kevinptu <99754489+kevinptu@users.noreply.github.com>
2026-09-25 12:57:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# comment
 import os 
 import ee
 import geemap
@@ -23,7 +22,7 @@ json_object = json.dumps(json_object)# Authorising the app
 credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
 
 # Initialize Google Earth Engine
-ee.Initialize(credentials, project='careful-ensign-420823')
+ee.Initialize(credentials, project='ee-landflux')
 
 # Set the title that appears on the browser tab
 st.set_page_config(page_title="KML/Shapefile/GeoJSON uploader")
@@ -84,8 +83,7 @@ def get_vector(uploaded_file, out_dir=None):
 
     return vector, out_name
 
-# def import_asset_to_gee(ee_object, asset_name, asset_path="projects/careful-ensign-420823/assets/ecoexplorer"):
-def import_asset_to_gee(ee_object, asset_name, asset_path="projects/ee-landflux/assets/private"):
+def import_asset_to_gee(ee_object, asset_name, asset_path="projects/ee-landflux/assets/public"):
     asset_id = f"{asset_path}/{asset_name}"
     exportTask = ee.batch.Export.table.toAsset(
         collection=ee_object,
